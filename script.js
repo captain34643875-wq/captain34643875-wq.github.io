b4cb8897fcb63f157d94942749d4c93a1b9f3065
captain34643875-wq <captain34643875@gmail.com>
2026-01-28 16:46:18 +0900

    Update

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 const themeBtn = document.getElementById("themeToggle");
 const langBtn = document.getElementById("langToggle");
 
-let isDark = false;  // 처음은 라이트
-let isKorean = false;
+let isDark = false;   // 처음은 라이트
+let isKorean = true; // 처음은 한국어
 
 /* 다크 / 라이트 */
-
 themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   isDark = !isDark;
   themeBtn.textContent = isDark ? "Light" : "Dark";
 });
 
-
-/* 한국어 / 영어 */
+/* 언어 토글 */
 langBtn.addEventListener("click", () => {
   isKorean = !isKorean;
-  langBtn.textContent = isKorean ? "EN" : "KR";
+  langBtn.textContent = isKorean ? "EN" : "KO";
 
   document.querySelectorAll("[data-ko]").forEach(el => {
     el.textContent = isKorean ? el.dataset.ko : el.dataset.en;
