33b2bdb7fe2bef099c72b2fcc2dcff295351d718
captain34643875-wq <captain34643875@gmail.com>
2026-01-28 16:34:08 +0900

    롤백

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const themeBtn = document.getElementById("themeToggle");
 const langBtn = document.getElementById("langToggle");
 
-let isDark = false;
-let isKorean = true;
+let isDark = true;
+let isKorean = false;
 
 /* 다크 / 라이트 */
 themeBtn.addEventListener("click", () => {
